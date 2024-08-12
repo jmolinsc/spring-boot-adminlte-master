028ce3159b1b1640a11c73ee13959961df668ad8
jmolinsc <centenojulio@hotmail.com>
2024-08-12 16:07:42 -0600

    NINGUNO

--- src/main/resources/static/dist/js/vta.js
+++ src/main/resources/static/dist/js/vta.js
@@ -3,35 +3,5 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
  */
 
-// A $( document ).ready() block.
-$(document).ready(function () {
-    debugger
-    $('#userTable').DataTable({
-        "processing": true,
-        "serverSide": true,
-        "ajax": "/api/users",
-        columns: [
-            {
-                data: 'firstName',
-            },
-            {
-                data: 'lastName',
-            },
-            {
-                data: 'username',
-                render: (data) => `<span class="shadow-sm px-2 py-1 rounded-3 bg-dark text-light">@${data}</span>`
-            },
-            {
-                data: 'email',
-                render: (data) => `<a class="text-dark" href="mailto: ${data}"  target="_blank">${data}</a>`
-            },
-            {
-                data: 'status',
-                render: (data) => `<span class="badge bg-${data ? 'success' : 'warning'}">${data ? 'active' : 'inactive'}</span>`
-            },
-        ]
-    });
-});
-
 
 
